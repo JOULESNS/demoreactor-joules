051613fb58ba82a663ab1b4dc49951412f72c0f8
Joules Neri <joulesns.25@gmail.com>
2023-03-24 20:59:43 -0500

    mi actualización

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,7 @@
 num1 = 5
 num2 = -2
 
-def suma(numero1, numero2):
+def producto(numero1, numero2):
     total = numero1 * numero2
     return total
 
@@ -9,7 +9,7 @@ def resta(numero1, numero2):
     total = numero1 - numero2
     return total
 
-resultado = suma(num1, num2)
+resultado = producto(num1, num2)
 resultado2 = resta(num1, num2)
 
-print('El resultado es: ',resultado2)
+print('El resultado del producto es es: ',resultado)
